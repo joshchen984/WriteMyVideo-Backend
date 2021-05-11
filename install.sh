303bcd47173cbcc2dcc51c87dde34f95b6866702
Josh Chen <joshchen984@gmail.com>
2021-05-11 14:21:02 -0700

    Make files in installation script

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 pip install -r requirements.txt
 docker run -p 8765:8765 --name gentle lowerquality/gentle
 docker stop gentle
+mkdir logs
+touch logs/app.log
+echo "SECRET_KEY: SOME_KEY" > config.yaml
